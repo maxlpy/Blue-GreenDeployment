e456fd466f05c6ace1f5e0114d86cea57e5d1940
maxlpy <maxlipengyu@gmail.com>
2015-04-06 15:28:10 -0400

    first commit

---
+++ blue-www/main.js
@@ -0,0 +1,59 @@
+var express = require('express')
+var fs      = require('fs')
+var multer = require('multer')
+var app = express()
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
+var args = process.argv.slice(2);
+var PORT = args[0];
+
+app.get('/', function(req, res) 
+{
+	res.send("Hello Blue2.");
+});
+
+app.get('/switch', function(req, res)
+{
+//	res.status(500).send('Something has broken!');
+	res.redirect('http://localhost:5060');
+//	console.log("switch to blue.");
+    
+	//res.redirect('/');
+	client.set("switch","1");
+	
+});
+
+app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+   console.log(req.body) // form fields
+   console.log(req.files) // form files
+
+   if( req.files.image )
+   {
+	   fs.readFile( req.files.image.path, function (err, data) {
+	  		if (err) throw err;
+	  		var img = new Buffer(data).toString('base64');
+			client.lpush("myPicture",img)
+	  		console.log(img);
+		});
+	}
+
+   res.status(204).end()
+}]);
+
+app.get('/meow', function(req, res) {
+	{
+		client.lpush("myPages",req.url)
+		client.lrange("myPicture",0,1,function(err,items){
+			var imagedata=items[0]
+			res.send("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+		})
+	}
+})
+
+var server = app.listen(3000, function () {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Example app listening at http://%s:%s', host, port)
+});
---
+++ green-www/main.js
@@ -0,0 +1,56 @@
+var express = require('express')
+var fs      = require('fs')
+var multer = require('multer')
+var app = express()
+var redis = require('redis')
+var client = redis.createClient(6380, '127.0.0.1', {})
+var args = process.argv.slice(2);
+var PORT = args[0];
+
+app.get('/', function(req, res) 
+{
+	res.send("Hello World, I am GREEN.");
+});
+
+app.get('/switch', function(req, res)
+{
+//	res.status(500).send('Something has broken! GREEN');
+	res.redirect('http://localhost:9090');
+//res.redirect('/');
+client.set("switch","1");
+});
+
+app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+   console.log(req.body) // form fields
+   console.log(req.files) // form files
+
+   if( req.files.image )
+   {
+	   fs.readFile( req.files.image.path, function (err, data) {
+	  		if (err) throw err;
+	  		var img = new Buffer(data).toString('base64');
+			client.lpush("myPicture",img)
+	  		console.log(img);
+		});
+	}
+
+   res.status(204).end()
+}]);
+
+app.get('/meow', function(req, res) {
+	{
+		client.lpush("myPages",req.url)
+		client.lrange("myPicture",0,1,function(err,items){
+			var imagedata=items[0]
+			res.send("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+		})
+	}
+})
+
+var server = app.listen(PORT, function () {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Example app listening at http://%s:%s', host, port)
+});
